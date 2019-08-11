2fc988888ed7a93e1149612ce9ed26dbfda53c5f
parfei <jenniferlowe7@LIVE.com>
2019-08-11 15:19:47 +1200

    Changed to Linux environment.

--- wiki_speak.sh
+++ wiki_speak.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
 
+#script variables
 select=""
-printMainMenu(){ #Print a neat menu, user chooses an option.
+path=`realpath wiki_speak.sh` #<-CHECK
 
-options=("(l)ist existing creations" "(p)lay an existing creation"
-"(d)elete an existing creation" "(c)reate a new creation"
-"(q)uit authoring tool")
+#functions
+printMainMenu()
+{
+  local options=("(l)ist existing creations" "(p)lay an existing creation"
+                  "(d)elete an existing creation" "(c)reate a new creation"
+                  "(q)uit authoring tool")
 
-echo "====================================================="
+echo "=========================================================="
 echo "Welcome to the Wiki-Speak Authoring Tool"
-echo "====================================================="
+echo "=========================================================="
 echo -n "Please select one of the following options: "
 for i in ${options[*]}
 do
   printf "\n\t%s" "${options[i]}"
 done
 
-wrongSelect=false
+local wrongSelect=false
 while [ select != "[lLpPdDcCqQ]" ]
 do
-  if [ wrongSelect ]; then
-    echo -n "You entered an invalid option, try again...\t"
+  if [ wrongSelect ]
+  then
+    echo -n "You entered an invalid option, try again..."
   fi
-  read -p "Enter a selection [l/p/d/c/q]: " select
+
+  read -p "Enter a selection [l/p/d/c/q]:" select
   wrongSelect=true
 done
 }
+
+listCreations()
+{
+
+}
+
+#main program
+
+if [ ! -e "wiki_speak_creations" ]
+then
+  mkdir "$path/wiki_speak_creations"
+fi
 
 while [ select != [qQ] ]
 do
 
 
-
-
-
+  select=""
+  printMainMenu
 
 done
 
